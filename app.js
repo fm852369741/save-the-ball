fb0a21568d3f3e5b80971b3a99c0fed093751ccb
fm852369741 <93235715+fm852369741@users.noreply.github.com>
2021-11-18 12:00:38 +0530

    radius projectile

--- app.js
+++ app.js
@@ -162,7 +162,7 @@ window.addEventListener("click", (e) => {
     new Projectile({
       x: player.x,
       y: player.y,
-      radius: window.innerHeight / 250,
+      radius: window.innerWidth / 250,
       color: player.radius,
       velocity: velocity,
     })
